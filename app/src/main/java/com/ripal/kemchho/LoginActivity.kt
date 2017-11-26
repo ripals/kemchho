4aec0a7417269f6c077a9278bf62a614b83ae40f
Ripal Shah <ripal.1021@gmail.com>
2017-11-26 20:12:16 +0530

    lesson-03

---
+++ app/src/main/java/com/ripal/kemchho/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.ripal.kemchho
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClocked(view: View) {
+
+
+    }
+
+    fun loginCreateUserBtnClicked(view: View) {
+
+
+    }
+}
--- app/src/main/java/com/ripal/kemchho/MainActivity.kt
+++ app/src/main/java/com/ripal/kemchho/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ripal.kemchho
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -31,6 +33,21 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
+
+    fun addChannelClicked(view: View) {
+
+
+    }
+
+    fun sendMessageBtnClicked(view: View) {
+
+
     }
 
 }
